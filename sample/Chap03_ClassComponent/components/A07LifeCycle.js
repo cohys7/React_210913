ec572c5fc56533a812fcccbb7d6214e309ca44c4
SHINSUNGJIN <pxp13716@gmail.com>
2021-09-12 17:27:24 +0900

    sample

---
+++ sample/Chap03_ClassComponent/components/A07LifeCycle.js
@@ -0,0 +1,34 @@
+
+import React, { Component } from 'react'
+
+export class A08LifeCycle extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            color: 'green'
+        };
+        console.log('1. Constructor');
+    }
+
+    changeColor = () => {
+        this.setState({color: 'orange'})
+    }
+
+    render() {
+        console.log('3. render()')
+        return (
+            <div>
+                <h3>A07 Life Cycle</h3>
+
+                color: {this.props.color} / {this.state.color}
+                <br />
+                
+                <button onClick={this.changeColor}>Click</button>
+            </div>
+        )
+    }
+}
+
+export default A08LifeCycle
---
+++ sample/Chap05_Style/components/A04StyledComponent.js
@@ -0,0 +1,18 @@
+
+// import i styled-components
+// import i reactstrap
+// import i react-icons
+
+import React from 'react'
+
+
+function A04StyledComponent() {
+    return (
+        <div>
+            <h3>A04 Styled Component</h3>
+
+        </div>
+    )
+}
+
+export default A04StyledComponent
