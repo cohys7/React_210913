0343ed5f6bacba6b88632b8fb06cb768225ab688
SHINSUNGJIN <pxp13716@gmail.com>
2021-09-16 10:12:21 +0900

    .

--- Chap05_Style/src/components/A04StyledComponent.js
+++ Chap05_Style/src/components/A04StyledComponent.js
@@ -7,6 +7,8 @@ import React from 'react'
 // import styled from 'styled-components'      // 기존 HTML 요소에 CSS를 적용해서 새로운 태그를 생성해 준다
 
 import {Box, Button} from './css/A04Styled'
+import {Button as Btn} from 'reactstrap'
+import { Md3DRotation } from "react-icons/md";
 /*
 const Box = styled.div`
     background-color: ${props => props.bgcolor || 'lightgray'};
@@ -41,6 +43,12 @@ function A04StyledComponent() {
             <Box bgcolor="gray">Box 안에 있는 컨텐츠</Box>
             <Button>Click 01</Button>
             <Button>Click 01</Button>
+            <br/> <br />
+
+            <Btn color="primary">CLICK</Btn>
+            <Btn color="danger">CLICK</Btn>
+            <br /><br />
+            <Md3DRotation style={{color: 'orange', fontSize: '30pt'}} />
         </div>
     )
 }
