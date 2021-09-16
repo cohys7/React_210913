01076c10b68a9dc1ec09f1213c580f1551562b6c
SHINSUNGJIN <pxp13716@gmail.com>
2021-09-16 15:56:54 +0900

    .

--- Chap08_Axios/src/components/A01Axios.js
+++ Chap08_Axios/src/components/A01Axios.js
@@ -1,23 +1,98 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// npm i axios
+import axios from 'axios'
 
 function A01Axios() {
     
     const baseURL = 'http://localhost:8080/contacts/';
     const [data, setData] = useState();
-    
+
+    const getContactList = () => {
+        // axios.get(baseURL, {params: {pageno: 1, pagesize: 10}})
+        // .then( resp => setData(resp.data) )
+        // .catch( error => setData(error) )
+        axios({
+            method: 'GET',
+            url: baseURL,
+            params: {params: {pageno: 1, pagesize: 10}},
+        })
+        .then( resp => setData(resp.data ))
+        .catch( error => setData(error) )
+    };
+
+    const getContactListAync = async() => {
+        // await axios.get(baseURL, {params: {pageno: 1, pagesize: 10}})
+        // .then( resp => setData(resp.data) )
+        // .catch( error => setData(error) )
+        try {
+            console.log('start')
+            let result = await axios.get(baseURL, {params: {pageno: 2, pagesize: 10}})
+            setData(result.data);
+
+            console.log('2page end')
+
+            result = await axios.get(baseURL, {params: {pageno: 3, pagesize: 10}})
+            setData(result.data);
+
+            console.log('3page end')
+        }catch(error) {
+            setData(error);
+        }
+
+    };
+
+    const getContact = (no) => {
+        axios.get(baseURL + no)                 // 'http://localhost:8080/contacts/100';
+        .then( resp => setData(resp.data) )
+        .catch( error => setData(error) )
+    };
+
+    const addContact = () => {
+        const data = {
+            "name":"강감찬",
+            "tel":"010-2222-3339",
+            "address":"서울시"
+        }
+        axios.post(baseURL, data)                 // 'http://localhost:8080/contacts/100';
+        .then( resp => setData(resp.data) )
+        .catch( error => setData(error) )
+    };
+
+    const updateContact = (no) => {
+        const data = {
+            "name":"강감찬",
+            "tel":"010-2222-1111",
+            "address":"Busan"
+        }
+        axios.put(baseURL + no, data)
+        .then( resp => setData(resp.data) )
+        .catch( error => setData(error) )
+    };
+
+    const deleteContact = (no) => {
+        axios.delete(baseURL + no)
+        .then( resp => setData(resp.data) )
+        .catch( error => setData(error) )
+    };
+
+    useEffect( () => {
+        getContactList();
+    }, [])
+
     return (
         <div>
             <h3>A01 Axios GET</h3>
-            <button>DATA LIST</button>
-            <button>DATA LIST ASYNC</button>
-            <button>GET</button>
-            <button>ADD</button>
-            <button>UPDATE</button>
-            <button>DELETE</button><br />
+            <button onClick={getContactList}>DATA LIST</button>
+            <button onClick={getContactListAync}>DATA LIST ASYNC</button>
+            <button onClick={ () => getContact(100) }>GET</button>
+            <button onClick={ addContact }>ADD</button>
+            <button onClick={ () => updateContact('1631775021508') }>UPDATE</button>
+            <button onClick={ () => deleteContact('1631775021508') }>DELETE</button><br />
             <br />
             
-            <textarea cols="100" rows="15"></textarea>
+            <textarea cols="100" rows="15" defaultValue={JSON.stringify(data, null, 4)}></textarea>
         </div>
     )
 }
